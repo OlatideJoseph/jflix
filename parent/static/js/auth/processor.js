8656204302b2751be52908d5a44dab6c100f9a16
Olatide Joseph <=olatidejoseph17@gmail.com>
2023-03-20 07:07:02 -0700

    form and accounts

--- parent/static/js/auth/processor.js
+++ parent/static/js/auth/processor.js
@@ -10,48 +10,71 @@ const validate_input= async (json_data)=>{
 	}
 }
 $(document).ready(function(){
-	        let form_length=$('form').children().length;
-	        alert(form_length);
-			let jsignup={
+    let form_length=$('form').children().length;
+    alert(form_length);
+	let jsignup={
 
-			}
-			var counter=0;
-			function validate_input(field){
-				if (field.attr('type')){
-					if (counter >=9){
+	}
+	var counter=0;
+	function validate_input(field){
+		if (field.attr('type')){
+			jsignup[field.attr('name')]=field.val()
+			console.log(jsignup);
+			return "inputs";
+			counter ++
+		}else if( !field.attr('type')){
+			field=field.children('select')
+			jsignup[field.attr('name')]=field.val()
+			console.log(jsignup);
+			return "others";
+			counter++;
+		}else{
+			console.info("incoming messages");
+			throw TypeError;
+		}
+	}
+	var btn = $('.field').hide();
+	var obj=$('#email-container');
+	$('#next-button').click(function(){
+		child=obj.children[1];
+		if (child.val() == null){
+			throw Error;
+		}
+		counter++;
+		if (counter >=7){
+			$(this).hide()
+			$('input[type="submit"]').show();
+		}else if(counter>=1){
+			$('#prev-btn').show();
+			$(this).show();
+		}else{
 
-
-
-					}else if(counter>1){
-						$('#prev-btn').show();
-					}else{
-						
-					}
-					jsignup[field.attr('name')]=field.val()
-					console.log(jsignup);
-					return "inputs";
-				}else if( !field.attr('type')){
-					field=field.children('select')
-					jsignup[field.attr('name')]=field.val()
-					console.log(jsignup);
-					return "others";
-				}else{
-					console.info("incoming messages");
-					throw TypeError;
-				}
-			}
-			var btn = $('.field').hide();
-			var obj=$('#email-container');
-			$('#next-button').click(function(){
-				if (validate_input(obj.children('input')) == "inputs"){
-					var next=obj.next(); //next function for the parent object
-					next.toggleClass('field-hidden'); // change the hidden field to the next object
-					obj.toggleClass('field-hidden');
-					obj=obj.next();
-				}else if (validate_input(obj) == "others"){
-					obj.toggleClass('field-hidden');
-					obj=obj.next();
-					obj.toggleClass('field-hidden');
-				}
-			});
-		});
+		}
+		if (validate_input(obj.children('input')) == "inputs"){
+			var next=obj.next(); //next function for the parent object
+			next.toggleClass('field-hidden'); // change the hidden field to the next object
+			obj.toggleClass('field-hidden');
+			obj=obj.next();
+		}else if (validate_input(obj) == "others"){
+			obj.toggleClass('field-hidden');
+			obj=obj.next();
+			obj.toggleClass('field-hidden');
+		}
+	});
+	$('#prev-btn').click(function (){
+		child=obj.children[1]
+		if (child.val() == null){
+			throw Error;
+		}
+		$('input[type="submit"]').hide();
+		if (counter<=1){
+            $(this).hide();
+            $('#next-button').show();
+		}
+		counter--;
+		obj.toggleClass('field-hidden');
+		obj=obj.prev();
+		obj.toggleClass('field-hidden');
+		$('#next-button').show();
+	})
+});
